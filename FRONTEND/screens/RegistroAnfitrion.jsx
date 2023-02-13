cda9917913eecb58c09f14dba133ee7e60cef2cc
Juan Suarez <jp.juansuarez@gmail.com>
2023-02-13 18:25:43 -0300

    Agregado estilos basicos de texto y formulario

--- FRONTEND/screens/RegistroAnfitrion.jsx
+++ FRONTEND/screens/RegistroAnfitrion.jsx
@@ -5,20 +5,34 @@ import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 
 const RegistroAnfitrion = () => {
+    const [text, onChangeText] = React.useState('');
     const isDarkMode = useColorScheme() === 'dark'
     return (
-        <View style={{ flex: 1, backgroundColor: isDarkMode ? Colors.black : Colors.white }}>
+        <View style={{ flex: 1, backgroundColor: isDarkMode ? Colors.black : Colors.white, padding: '5%' }}>
             <View>
                 <Text style={Styles.RegistroTitle}>Registro</Text>
-                <Text>Crea una cuenta para acceder a todas las funcionalidades de Mascotas Cuidadas!</Text>
+                <Text style={Styles.RegistroSub}>Crea una cuenta para acceder a todas las funcionalidades de Mascotas Cuidadas!</Text>
             </View>
             <View>
-                <Text>Email</Text>
-                <TextInput></TextInput>
-                <Text>Tu nombre</Text>
-                <TextInput></TextInput>
-                <Text>Tu contraseña</Text>
-                <TextInput></TextInput>
+                <Text style={Styles.formTitles}>Email</Text>
+                <TextInput
+                    style={Styles.input}
+                    onChangeText={onChangeText}
+                    value={text}
+                    placeholder="Ex: abc@example.com "/>
+                <Text style={Styles.formTitles}>Tu nombre</Text>
+                <TextInput
+                    style={Styles.input}
+                    onChangeText={onChangeText}
+                    value={text}
+                    placeholder="Ex. Saul Ramirez"/>
+                <Text style={Styles.formTitles}>Tu contraseña</Text>
+                <TextInput
+                    style={[Styles.input, {marginBottom: 0}]}
+                    onChangeText={onChangeText}
+                    value={text}
+                    placeholder="*******"/>
+                <Text style={{marginBottom: 12}}>Mínimo ingresa 7 caracteres</Text>
             </View>
             <View>
                 <Button title='Registrarme'/>
@@ -33,9 +47,31 @@ const RegistroAnfitrion = () => {
 
 const Styles = StyleSheet.create ({
     RegistroTitle: {
-        fontFamily: 'Inter',
-        color: 'red'
-    }
-
+        color: '#FF5E5E',
+        fontWeight: 700,
+        fontSize: 32,
+        // fontFamily: 'Inter'
+    },
+    RegistroSub: {
+        color: 'black',
+        fontWeight: 700,
+        fontSize: 16,
+        marginBottom: 12
+        // fontFamily: 'Roboto'
+    },
+    formTitles: {
+        color: 'black',
+        fontWeight: 400,
+        fontSize: 16,
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1.5,
+        borderColor: '#575DFB',
+        borderRadius: 16,
+        padding: 10,
+    },
 })
+
 export default RegistroAnfitrion;
