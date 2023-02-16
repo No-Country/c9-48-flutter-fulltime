3c4d3b688ebbde1fe04263f90256c7a4156f978c
JuanSuarez <jp.juansuarez@gmail.com>
2023-02-16 13:34:51 -0300

    Se arreglan estilos de la pantalla Bienvenidos

--- FRONTEND/screens/Bienvenidos.jsx
+++ FRONTEND/screens/Bienvenidos.jsx
@@ -5,36 +5,30 @@ import CommonButton from '../components/CommonButton';
 
 
 const Bienvenidos = ({navigation}) => {
-    const isDarkMode = useColorScheme() === 'dark'
+
     return (
-        <View style={[styles.container, { backgroundColor: isDarkMode ? Colors.black : Colors.white, }]}>
-            <View style={{ position: "relative",  flexDirection: "column", alignSelf: "flex-start", padding: '5%' }} >
-                <Button
+        <View style={styles.container}>
+            <View style={{alignSelf: 'flex-start' }}>
+                <Button 
                     title='Atrás'
                     color='grey'
                     onPress={() => navigation.navigate('CarouselIntro')}
                 />
-            </View>
-            <View style={styles.logoContainer}>
+                </View>
                 <Image
-                    style={styles.logo}
+                    resizeMode='center'
                     source={require('../assets/Logo.png')} />
-            </View>
-            <View>
+            <View style={styles.ButtonContainer}>
                 <CommonButton
                     buttonText={'Crear Cuenta'}
                     onPress={() => navigation.navigate('RegistroOpcion')}
                 />
-
-            </View>
-            <View >
                 <CommonButton
                     buttonText={'Inicia Sesión'}
                     ButtonColor={style.blue}
                     onPress={() => navigation.navigate('InicioSesion')}
                 />
             </View>
-            
         </View>
     )
 }
@@ -42,17 +36,15 @@ const Bienvenidos = ({navigation}) => {
 const styles = StyleSheet.create ({
     container: {
         flex: 1,
-    },
-    logoContainer: {
-        justifyContent: 'center',
+        padding: '5%',
         alignItems: 'center',
-
+        justifyContent: 'flex-start',
+        // backgroundColor: 'red'
     },
-    logo: {
-    width: '65%',
-    height: '65%',
-    },
-
+    ButtonContainer: {
+        padding: 20, 
+        // backgroundColor: 'green'
+    }
 
 });
 
