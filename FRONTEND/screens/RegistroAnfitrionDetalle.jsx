45b36ab5915351708b074bfaf1a59d74b483c76a
JuanSuarez <jp.juansuarez@gmail.com>
2023-02-16 17:15:50 -0300

    Agregado y actualizado seleccionadores de pais y ciudad.

--- FRONTEND/screens/RegistroAnfitrionDetalle.jsx
+++ FRONTEND/screens/RegistroAnfitrionDetalle.jsx
@@ -5,6 +5,8 @@ import { SelectList } from 'react-native-dropdown-select-list';
 import  DatePicker  from 'react-native-date-picker'
 import CalendarRegister from '../components/CalendarRegister'
 import CommonButton from '../components/CommonButton';
+import CountryPicker from '../components/CountryPicker';
+import CityPicker from '../components/CityPicker';
 
 
 
@@ -12,7 +14,8 @@ import CommonButton from '../components/CommonButton';
 
 const RegistroAnfitrionDetalle = ({ navigation }) => {
     const Separator = () => <View style={Styles.separator} />;
-    const [selected, setSelected] = React.useState('');
+    const [selectedAnfPais, setSelectedAnfPais] = React.useState('');
+    const [selectedAnfCiudad, setSelectedAnfCiudad] = React.useState('');
     
     const isDarkMode = useColorScheme() === 'dark'
     
@@ -23,49 +26,17 @@ const RegistroAnfitrionDetalle = ({ navigation }) => {
     const [fechaEgreso, setFechaEgreso] = React.useState("DD/MM/AAAA")
     
 
-    const dataCountries = [
-        { key: '1', value: 'Argentina'},
-        { key: '2', value: 'Bolivia' },
-        { key: '3', value: 'Brazil' },
-        { key: '4', value: 'Chile'},
-        { key: '5', value: 'Colombia' },
-        { key: '6', value: 'Ecuador' },
-        { key: '7', value: 'Paraguay' },
-        { key: '8', value: 'Uruguay'},
-        { key: '9', value: 'Venezuela' },
-    ]
-    const dataCities = [
-        { key: '1', value: 'A' },
-        { key: '2', value: 'B' },
-        { key: '3', value: 'C' },
-        { key: '4', value: 'D' },
-        { key: '5', value: 'E' },
-        { key: '6', value: 'F' },
-        { key: '7', value: 'G' },
-        { key: '8', value: 'H' },
-        { key: '9', value: 'I' },
-    ]
+
     return (
         <View style={{ flex: 1, backgroundColor: isDarkMode ? Colors.black : Colors.white, padding: '5%' }}>
             <View>
                 <Text style={Styles.RegistroTitle}>Encontrá un cuidador/a</Text>
                 <Text style={Styles.DesdeHasta}>Ubicación</Text>
                 <Text style={Styles.formTitles}>País</Text>
-                    <SelectList
-                        setSelected={(val) => setSelected(val)}
-                        data={dataCountries}
-                        save="value"
-                        placeholder='Seleccioná el país...'
-                        searchPlaceholder='Buscar'
-                    />
+                <CountryPicker setSelected={setSelectedAnfPais}/>
                 <Text style={Styles.formTitles}>Ciudad</Text>
-                    <SelectList
-                        setSelected={(val) => setSelected(val)}
-                        data={dataCities}
-                        save="value"
-                        placeholder='Seleccioná la ciudad...'
-                        searchPlaceholder='Buscar'
-                    />
+                <CityPicker  setSelected={setSelectedAnfCiudad} />
+
                 <Separator/>
             </View>
             <View>
@@ -78,7 +49,8 @@ const RegistroAnfitrionDetalle = ({ navigation }) => {
                 <Text style={Styles.RegistroSub}>Ingresá la fecha de cierre en la que deseas que buscas cuidador.</Text>
                 <CalendarRegister date={dateTo} setDate={setDateTo} buttonTitle={fechaEgreso} setButtonTitle={setFechaEgreso} />
                 <Separator/>
-
+                <Text>Pais Seleccionado {selectedAnfPais}</Text>
+                <Text>Ciudad Seleccionada {selectedAnfCiudad}  </Text>
                 <Text>Fecha de Ingreso {fechaIngreso}</Text>
                 <Text>Fecha de Egreso {fechaEgreso}</Text>
             </View>
