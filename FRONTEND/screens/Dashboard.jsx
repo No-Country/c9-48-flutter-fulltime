38a6fa244c459eb58280b641fee3a4c1f60088a5
JuanSuarez <jp.juansuarez@gmail.com>
2023-02-24 18:19:03 -0300

    Se crea componente para navegacion

--- FRONTEND/screens/Dashboard.jsx
+++ FRONTEND/screens/Dashboard.jsx
@@ -1,47 +1,53 @@
-import { Text, StyleSheet, ScrollView } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import Separator from "../components/Separator";
 import DashboardPerfil from "../components/DashboardPerfil";
+import TabsMascotasBar from "../components/BottomNav";
+
 
 const Dashboard = () => {
+
     return (
-        <ScrollView contentContainerStyle={styles.container}>
-            <Text>Buscador🔍</Text>
-            <Separator/>
-            <Text>Cuidadores</Text>
-            <Text>Casas</Text>
-            <Separator/>
-            <DashboardPerfil
-            fotoPerfil={'Soy una foto de perfil'}
-            descripcionPerfil={'Hola me llamo Carla soy viajera y amante de los animales. Mi sueño es recorrer todo Latam.'}
-            identidadVerificada={"✅ Identidad verificada"}
-            />
-            <DashboardPerfil
-            fotoPerfil={'Soy una foto de perfil 2'}
-            descripcionPerfil={'Desde chico crecí con dos perros y soy adiestrador de perros profesional. Soy Mexicano.'}
-            identidadVerificada={"✅ Identidad verificada"}
-            />
-            <DashboardPerfil
-            fotoPerfil={'Soy una foto de perfil 3'}
-            descripcionPerfil={'Amo viajar sola y no se me ocurre forma que estando acompañada de mascotas.'}
-            identidadVerificada={"✅ Identidad verificada"}
-            />
-            <DashboardPerfil
-            fotoPerfil={'Soy una foto de perfil 4'}
-            descripcionPerfil={'Estoy recorriendo varias ciudades de México el mes próximo. Amo las mascotas.'}
-            identidadVerificada={"✅ Identidad verificada"}
-            />
-            <DashboardPerfil
-            fotoPerfil={'Soy una foto de perfil 5'}
-            descripcionPerfil={'Descrpcion 5'}
-            identidadVerificada={"✅ Identidad verificada"}
-            />
-            <DashboardPerfil
-            fotoPerfil={'Soy una foto de perfil 6'}
-            descripcionPerfil={'Descrpcion 6'}
-            identidadVerificada={"✅ Identidad verificada"}
-            />
-            
-        </ScrollView>
+        <View>
+            <ScrollView contentContainerStyle={styles.container}>
+                <Text>Buscador🔍</Text>
+                <Separator/>
+                <Text>Cuidadores</Text>
+                <Text>Casas</Text>
+                <Separator/>
+                <DashboardPerfil
+                fotoPerfil={'Soy una foto de perfil'}
+                descripcionPerfil={'Hola me llamo Carla soy viajera y amante de los animales. Mi sueño es recorrer todo Latam.'}
+                identidadVerificada={"✅ Identidad verificada"}
+                />
+                <DashboardPerfil
+                fotoPerfil={'Soy una foto de perfil 2'}
+                descripcionPerfil={'Desde chico crecí con dos perros y soy adiestrador de perros profesional. Soy Mexicano.'}
+                identidadVerificada={"✅ Identidad verificada"}
+                />
+                <DashboardPerfil
+                fotoPerfil={'Soy una foto de perfil 3'}
+                descripcionPerfil={'Amo viajar sola y no se me ocurre forma que estando acompañada de mascotas.'}
+                identidadVerificada={"✅ Identidad verificada"}
+                />
+                <DashboardPerfil
+                fotoPerfil={'Soy una foto de perfil 4'}
+                descripcionPerfil={'Estoy recorriendo varias ciudades de México el mes próximo. Amo las mascotas.'}
+                identidadVerificada={"✅ Identidad verificada"}
+                />
+                <DashboardPerfil
+                fotoPerfil={'Soy una foto de perfil 5'}
+                descripcionPerfil={'Descrpcion 5'}
+                identidadVerificada={"✅ Identidad verificada"}
+                />
+                <DashboardPerfil
+                fotoPerfil={'Soy una foto de perfil 6'}
+                descripcionPerfil={'Descrpcion 6'}
+                identidadVerificada={"✅ Identidad verificada"}
+                />
+            </ScrollView>
+            {/* <TabsMascotasBar/> */}
+
+        </View>
     )
 
 }
