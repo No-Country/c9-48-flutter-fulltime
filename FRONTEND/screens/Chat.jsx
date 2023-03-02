5beacdb9852a49b46b7201eff2e79b244b3f08e6
Juan Suarez <jp.juansuarez@gmail.com>
2023-03-02 13:41:53 -0300

    Agregada pantalla chat

---
+++ FRONTEND/screens/Chat.jsx
@@ -0,0 +1,27 @@
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+
+
+
+const Chat  = () => {
+
+    return (
+        <View styles={styles.container}>
+                <Text style={{alignSelf: 'center', alignContent: 'center'}}>🚧Chat en construccion🚧</Text>   
+        </View>
+    )
+
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: '5%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        // backgroundColor: 'pink'
+    },
+
+})
+
+
+export default Chat;
+
